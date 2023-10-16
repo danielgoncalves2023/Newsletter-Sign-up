68f0b100fba54c10c57ec0f140a60afd2cd67364
Daniel Gonçalves <140750916+danielgoncalves2023@users.noreply.github.com>
2023-10-16 17:34:11 -0300

    validação e responsividade concluidas

---
+++ assets/script/script.js
@@ -0,0 +1,38 @@
+const pageCadastro = document.getElementById('container');
+const pageSucesso = document.getElementById('container_sucesso');
+var displayGrid; // Desktop
+var displayFlex; // Mobile
+
+function inscreverNewsletter(event) {
+    event.preventDefault();
+
+    var novoEmail = document.getElementById('email_cadastro');
+    var emailCadastrado = document.getElementById('emailCadastrado');
+    emailCadastrado.innerHTML = novoEmail.value;
+
+    // Validação de Email
+    if (novoEmail.value.indexOf('@') == -1 || novoEmail.value.indexOf('.') == -1) {
+        var textoErroEmail = document.getElementById('erro_email');
+
+        // Aviso email inválido
+        textoErroEmail.innerHTML = "Favor digitar um email válido."
+        textoErroEmail.style.color = 'red';
+        novoEmail.style.color = 'red';
+        novoEmail.style.border = '2px solid red';
+
+    } else {
+        if (pageCadastro.style.display == 'grid') {
+            displayGrid = true;
+        } else {
+            displayFlex = true;
+        }
+
+    // Transição de página cadastro, para cadastro finalizado
+    pageCadastro.style.display = 'none';
+    pageSucesso.style.display = 'flex';
+    }
+}
+
+function fechar() {
+    location.reload();
+}
