ab06b8f5e8c105d53a7505459a2a35bfc62cb292
Daniel Gonçalves <140750916+danielgoncalves2023@users.noreply.github.com>
2023-10-16 13:04:10 -0300

    Breakpoints and background images

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
+const pageCadastro = document.getElementById('container');
+const pageSucesso = document.getElementById('container_sucesso');
+var displayGrid; // Desktop
+var displayFlex; // Mobile
 
-var pageCadastro = document.getElementById('container');
-var pageSucesso = document.getElementById('container_sucesso');
-// Coletando input de email cadastrado:
-const emailCadastrado = document.querySelector('#email_cadastro');
+function inscreverNewsletter(event) {
+    event.preventDefault();
 
-function submit() {
-
-    // Confirmação de email:
-    const confirmacaoEmail = document.getElementById('emailCadastrado');
-    // Mostrar email que foi enviado confirmação de cadastro:
-    confirmacaoEmail.innerHTML = `${emailCadastrado}`;
+    const novoEmail = document.getElementById('email_cadastro').value;
+    const emailCadastrado = document.getElementById('emailCadastrado');
+    emailCadastrado.innerHTML = novoEmail;
+    
+    if (pageCadastro.style.display == 'grid') {
+        displayGrid = true;
+    } else {
+        displayFlex = true;
+    }
 
     pageCadastro.style.display = 'none';
     pageSucesso.style.display = 'flex';
 }
 
 function fechar() {
-    pageCadastro.style.display = 'grid';
+    if (displayGrid == true) {
+        pageCadastro.style.display = 'grid';
+    } else if (displayFlex == true) {
+        pageCadastro.style.display = 'flex';
+    }
+
     pageSucesso.style.display = 'none';
+
+    novoEmail = "";
 }
-
-
-
-document.getElementById('submit').addEventListener("click", submit())
